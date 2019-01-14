000fa1a93f16e5ca3a243029a558f079a8974448
Caleb Rowe <cajrowe@iu.edu>
2019-01-14 15:59:07 -0500

    home page done, working on about us, all need responsiveness

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+
+$(document).ready(function(){
+	//link click events, scrolls to page on screen
+	$("#story-link").click(function(){
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $(".section-story").offset().top
+		}, 1000);
+	});
+	
+	$("#guide-link").click(function(){
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $(".section-guides").offset().top
+		}, 1000);
+	});
+	
+	$("#mission-link").click(function(){
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $(".section-mission").offset().top
+		}, 1000);
+	});
+	
+	
+});
+	
+	
